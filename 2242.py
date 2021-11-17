0df757e033d35b5bb2affdb9ac9d8c399bdddff1
OtavioDaflonC <94200768+OtavioDaflonC@users.noreply.github.com>
2021-11-17 16:08:22 -0300

    Create 2242.py

---
+++ 2242.py
@@ -0,0 +1,10 @@
+#2242
+entrada = input()
+vogais = ""
+for l in entrada:
+    if(l=="a" or l=="e" or l=="i" or l=="o" or l=="u"):
+        vogais+=l
+if(vogais == vogais[::-1]):
+    print("S")
+else:
+    print("N")
